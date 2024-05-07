cef78aa577cc9c7ecd0efbcf205e0644e0ad385c
ManuGoa <manuelgonzalezalejo5@gmail.com>
2024-05-07 04:53:30 -0300

    falta dejar un tiempo despues de la respuesta. Ademas modificar el funcionamineto del comodin

--- QuizMaster/app/src/main/java/com/example/quizmaster/SegundaActividad.kt
+++ QuizMaster/app/src/main/java/com/example/quizmaster/SegundaActividad.kt
@@ -1,5 +1,6 @@
 package com.example.quizmaster
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.TextView
 import androidx.activity.ComponentActivity
@@ -7,72 +8,131 @@ import org.json.JSONArray
 import org.json.JSONObject
 import java.io.IOException
 import android.util.Log
+import android.graphics.Color
+import android.widget.Button
+import android.widget.ImageButton
 
 
 class SegundaActividad : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_segunda_actividad)
 
-        val categoriaSeleccionada = intent.getStringExtra("categoria")
-        val textViewCategoria = findViewById<TextView>(R.id.textViewCategoria)
-        textViewCategoria.text = categoriaSeleccionada
+        private var preguntasRestantes = 3
+        private var seUsoComodin = false
+        private val preguntasSeleccionadas = mutableSetOf<JSONObject>()
+        private lateinit var botonesOpcion: List<TextView>
+        private var opcionCorrectaIndex: Int? = null
+        private var preguntasCategoria = mutableListOf<JSONObject>()
 
-        val jsonStr: String? = loadJSONFromAsset("preguntas.json")
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_segunda_actividad)
 
-        if (jsonStr != null && categoriaSeleccionada != null) {
-            try {
-                val preguntasCategoria = mutableListOf<JSONObject>()
+            val categoriaSeleccionada = intent.getStringExtra("categoria")
+            val textViewCategoria = findViewById<TextView>(R.id.textViewCategoria)
+            textViewCategoria.text = categoriaSeleccionada
 
-                val jsonObject = JSONObject(jsonStr)
-                val keys = jsonObject.keys()
+            botonesOpcion = listOf(findViewById(R.id.opcion0), findViewById(R.id.opcion1), findViewById(R.id.opcion2), findViewById(R.id.opcion3))
 
-                while (keys.hasNext()) {
-                    val categoria = keys.next()
-                    if (categoria == categoriaSeleccionada) {
-                        val preguntasCategoriaArray = jsonObject.getJSONArray(categoria)
-                        for (j in 0 until preguntasCategoriaArray.length()) {
-                            preguntasCategoria.add(preguntasCategoriaArray.getJSONObject(j))
-                            Log.d("Se agrego un preg", preguntasCategoriaArray.getJSONObject(j).toString())
+            val jsonStr: String? = loadJSONFromAsset("preguntas.json")
+
+            if (jsonStr != null && categoriaSeleccionada != null) {
+                try {
+
+                    val jsonObject = JSONObject(jsonStr)
+                    val keys = jsonObject.keys()
+
+                    while (keys.hasNext()) {
+                        val categoria = keys.next()
+                        if (categoria == categoriaSeleccionada) {
+                            val preguntasCategoriaArray = jsonObject.getJSONArray(categoria)
+                            for (j in 0 until preguntasCategoriaArray.length()) {
+                                preguntasCategoria.add(preguntasCategoriaArray.getJSONObject(j))
+                                Log.d(
+                                    "Se agrego un preg",
+                                    preguntasCategoriaArray.getJSONObject(j).toString()
+                                )
+                            }
                         }
                     }
-                }
 
-                val preguntaSeleccionada = preguntasCategoria.random()
-                val preguntaText = preguntaSeleccionada.getString("question")
-                val opcionesArray = preguntaSeleccionada.getJSONArray("options")
-                val opcionCorrectaIndex = preguntaSeleccionada.getInt("correctAnswerIndex")
-                val opcionCorrecta = opcionesArray.getString(opcionCorrectaIndex)
+                    // Mostrar una pregunta inicial
+                    mostrarSiguientePregunta(preguntasCategoria)
+
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+
+        private fun mostrarSiguientePregunta(preguntasCategoria: List<JSONObject>) {
+            if (preguntasRestantes > 0) {
+                restablecerColoresBotones()
+                var preguntaSeleccionada: JSONObject? = null
+                do {
+                    preguntaSeleccionada = preguntasCategoria.random()
+                } while (preguntasSeleccionadas.contains(preguntaSeleccionada))
+
+                val preguntaText = preguntaSeleccionada?.getString("question")
+                val opcionesArray = preguntaSeleccionada?.getJSONArray("options")
+                opcionCorrectaIndex = preguntaSeleccionada?.getInt("correctAnswerIndex") ?: -1
 
                 val textViewPregunta = findViewById<TextView>(R.id.textViewPregunta)
                 textViewPregunta.text = preguntaText
 
-                val btnOpcion0 = findViewById<TextView>(R.id.opcion0)
-                val btnOpcion1 = findViewById<TextView>(R.id.opcion1)
-                val btnOpcion2 = findViewById<TextView>(R.id.opcion2)
-                val btnOpcion3 = findViewById<TextView>(R.id.opcion3)
+                botonesOpcion.forEachIndexed { index, button ->
+                    button.text = opcionesArray?.getString(index)
 
-                btnOpcion0.text = opcionesArray.getString(0)
-                btnOpcion1.text = opcionesArray.getString(1)
-                btnOpcion2.text = opcionesArray.getString(2)
-                btnOpcion3.text = opcionesArray.getString(3)
+                    button.setOnClickListener {
+                        comprobarRespuesta(index)
+                    }
+                }
 
-                // aca se verifica la respuesta
-            } catch (e: Exception) {
-                e.printStackTrace()
+                val btnComodin = findViewById<ImageButton>(R.id.comodin)
+                btnComodin.setOnClickListener {
+                    if (!seUsoComodin) {
+                        seUsoComodin = true
+                        preguntasRestantes++
+                        btnComodin.setBackgroundColor(Color.YELLOW)//una vez que se usa el comodin lo cambio de color
+                    }
+                }
+
+                if (preguntaSeleccionada != null) {
+                    preguntasSeleccionadas.add(preguntaSeleccionada)
+                }
+                preguntasRestantes--
+            } else {
+                // puede salir un toast
             }
         }
+
+        private fun comprobarRespuesta(opcionSeleccionada: Int) {
+            if (opcionSeleccionada != opcionCorrectaIndex) {
+                botonesOpcion[opcionSeleccionada].setBackgroundColor(Color.GRAY)
+            }
+            botonesOpcion[opcionCorrectaIndex!!].setBackgroundColor(Color.GREEN)
+            mostrarSiguientePregunta(preguntasCategoria)
+        }
+
+        // Método para cargar el archivo JSON
+        private fun loadJSONFromAsset(fileName: String): String? {
+            var json: String? = null
+            try {
+                val inputStream = assets.open(fileName)
+                json = inputStream.bufferedReader().use { it.readText() }
+            } catch (ex: IOException) {
+                ex.printStackTrace()
+            }
+            return json
+        }
+    private fun restablecerColoresBotones() {
+        val opcionCero = findViewById<Button>(R.id.opcion0)
+        val opcionUno = findViewById<Button>(R.id.opcion1)
+        val opcionDos = findViewById<Button>(R.id.opcion2)
+        val opcionTres = findViewById<Button>(R.id.opcion3)
+        opcionCero.setBackgroundResource(R.color.orange)
+        opcionUno.setBackgroundResource(R.color.orange)
+        opcionDos.setBackgroundResource(R.color.orange)
+        opcionTres.setBackgroundResource(R.color.orange)
+        }
     }
 
-    // esto carga el string :)
-    private fun loadJSONFromAsset(fileName: String): String? {
-        var json: String? = null
-        try {
-            val inputStream = assets.open(fileName)
-            json = inputStream.bufferedReader().use { it.readText() }
-        } catch (ex: IOException) {
-            ex.printStackTrace()
-        }
-        return json
-    }
-}
