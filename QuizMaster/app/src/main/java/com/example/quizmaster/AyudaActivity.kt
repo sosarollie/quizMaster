b26cd3dc3d33a4d0f21953ae4042959f5051595a
ManuGoa <manuelgonzalezalejo5@gmail.com>
2024-05-27 04:39:10 -0300

    boton volver de la activity de ayuda

--- QuizMaster/app/src/main/java/com/example/quizmaster/AyudaActivity.kt
+++ QuizMaster/app/src/main/java/com/example/quizmaster/AyudaActivity.kt
@@ -1,6 +1,8 @@
 package com.example.quizmaster
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +12,11 @@ class AyudaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ayuda)
+        val volver=findViewById<Button>(R.id.volver)
+        volver.setOnClickListener(){
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
